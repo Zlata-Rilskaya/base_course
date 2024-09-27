691271a1123800d136af843ae4d023fc72723938
Zlata-Rilskaya <rylskaazlata@gmail.com>
2024-09-27 19:47:50 +0000

    Домашняя работа №1

---
+++ task_4.py
@@ -0,0 +1,19 @@
+a = [1, 5, "Good", "Bad"]
+b = [9, "Blue", "Red", 11]
+print(a[1])
+print(b[3])
+print(5 + 11)
+
+print(a[2])
+print(b[2])
+print("Good" + "Red")
+
+print(a[0])
+print(b[0])
+print(1 * 9)
+
+print(a[1])
+print(b[3])
+print(5 ** 11)
+
+print(a + b)
