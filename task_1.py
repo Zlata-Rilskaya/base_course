3f8b2329b3f638ceac819656c5ba5d1313e5eca6
Zlata-Rilskaya <rylskaazlata@gmail.com>
2024-10-24 19:49:20 +0000

    дз

---
+++ task_1.py
@@ -0,0 +1,19 @@
+gravity_constant = 6.67408 * 10**(-11)
+
+speed_of_light = 3 * 10**8 
+
+acceleration_of_gravity = 9,8 
+
+Plancks_constant = 6,626070 * 10**-34 
+
+Avogadro_number = 6,221408211 * 1023
+
+electron_mass = 0,91093818872 * 10**-30 
+
+proton_mass = 1,672621513 * 10**-27 
+
+neutron_mass= 1,6749271613 * 10**-27 
+
+boltzmann_constant = 1,380649 * 10**23 
+
+Atomic_mass_unitе = 1,6605655*10-27 
---
+++ task_2.py
@@ -0,0 +1,8 @@
+import numpy as np 
+
+import task_1
+
+print(task_1.acceleration_of_gravity)
+from task_1 import acceleration_of_gravity as g
+
+
